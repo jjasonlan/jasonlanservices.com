47f8fc07e10ebf5e8ad8556252d07caeb3e1f401
jason-lan-qualia <66968365+jason-lan-qualia@users.noreply.github.com>
2021-09-05 11:31:42 -0700

    Updates

---
+++ precache-manifest.966aa41a267b3e5ca5b43bb0eb865fc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d65d90c22252ad0488f661ec92b37b60",
+    "url": "/jasonlanservices.com/index.html"
+  },
+  {
+    "revision": "7b39455260af96806461",
+    "url": "/jasonlanservices.com/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "e59b7c34775e5b7d61e9",
+    "url": "/jasonlanservices.com/static/js/2.5956dce1.chunk.js"
+  },
+  {
+    "revision": "7b39455260af96806461",
+    "url": "/jasonlanservices.com/static/js/main.dad625d3.chunk.js"
+  },
+  {
+    "revision": "f65116ae5607aae00e0a",
+    "url": "/jasonlanservices.com/static/js/runtime-main.96e7d4ac.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/jasonlanservices.com/static/media/logo.25bf045c.svg"
+  }
+]);
